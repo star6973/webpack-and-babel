7ee39b3a8bb6db5813c34b1c5f27b1d9456cacd7
MH.Ji <battlesun99@gmail.com>
2021-06-30 18:24:32 +0900

    webpack - 4, loader

--- source/index.js
+++ source/index.js
@@ -2,5 +2,7 @@
 // hello.js, world.js를 index.js에 bundling
 import hello_word from "./hello.js"
 import world_word from "./world.js"
+import css from "./style.css"
 
 document.querySelector("#root").innerHTML = hello_word + ' ' + world_word;
+console.log("css = ", css);
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // 확장자가 css 파일만을 확인하는 정규표현식
+                use: [
+                    'style-loader', // 가져온 css 파일 코드를 웹페이지 안에 style tag로 주입해주는 loader
+                    'css-loader' // css 파일을 읽어와서 webpack으로 가져오는 loader
+                ]
+            }
+        ]
     }
 }
